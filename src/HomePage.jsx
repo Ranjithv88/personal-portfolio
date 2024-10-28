920aa3c456c77adc90b6828bf4fdfe7f1bce85a2
ranjithv88 <ranjithkumar22445588@gmail.com>
2024-10-28 13:13:08 +0530

    fix problem

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useEffect, useState } from 'react'
+import React, { createContext, useEffect, useRef, useState } from 'react'
 import './HomePage.scss'
 import NavBar from './components/NavBar'
 import Home from './components/Home'
@@ -6,33 +6,34 @@ import Project from './components/Project'
 import Skill from './components/Skill'
 import Education from './components/Education'
 import Footer from './components/Footer'
-import { BiSleepy } from 'react-icons/bi'
-import { div } from 'framer-motion/client'
 
 export const Scroll = createContext()
 
 function HomePage() {
-  const [value,setValue] = useState(0)
-  // const HomeDiv = document.getElementsByClassName("HomePage")
-  let timeoutId
+  const [value, setValue] = useState(0)
+  let timeoutId = null
+  
   useEffect(() => {
     const homePage = document.getElementsByClassName('scroll')
     homePage[value].scrollIntoView({ block: 'start' })
-    // timeoutId = setTimeout(() => {
-    //   console.log(' working timeout.... ')
-    //   setValue(prevValue => (prevValue >= 4 ? 0 : prevValue + 1))
-    // }, 90000)
-    // return () => { console.log(' working clearUp.... ');clearTimeout(timeoutId) }
+    timeoutId = setTimeout(() => {
+      console.log(' working timeout.... ')
+      setValue(prevValue => (prevValue >= 4 ? 0 : prevValue + 1))
+    }, 2000)
+    return () => { console.log(' working clearUp.... '); clearTimeout(timeoutId) }
   }, [value])
-  // const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms))
-  // HomeDiv[0].EventListener("mousemove",simple())
-  // async function simple(){
-  //   console.log(' working sleep.... ')
-  //   clearTimeout(timeoutId)
-  //   await sleep(30000)
-  // }
+
+const handleTimeOut = ()=>{
+  if(timeoutId !== null){
+    clearTimeout(timeoutId)
+  }
+  timeoutId = setTimeout(() => {
+        console.log(' working timeout.... ')
+        setValue(prevValue => (prevValue >= 4 ? 0 : prevValue + 1))
+      }, 4000)
+}
   return (
-    <div className='HomePage'>
+    <div className='HomePage' onClick={handleTimeOut}>
       <Scroll.Provider value={{ setValue }}>
         <NavBar />
         <Home />
