209498ef37325cc5615a585a124aaef70404c7ac
Ranjithv88 <ranjithkumar22445588@gmail.com>
2024-12-06 09:46:25 +0530

    add new theme

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,6 +5,7 @@ import { motion } from 'framer-motion'
 import { FaSun } from "react-icons/fa6"
 import { IoLeaf } from "react-icons/io5"
 import { FaCloud } from "react-icons/fa"
+import { GiRose } from "react-icons/gi"
 import { MdNightlight } from "react-icons/md"
 
 function NavBar() {
@@ -23,12 +24,16 @@ function NavBar() {
             setThemeColorConditional(<FaSun/>)
         } else if(number === 2){
             bg = 'rgba(248, 233, 214, 1)'
-            text = 'rgb(20, 115, 95)'
+            text = 'rgba(20, 115, 95, 1)'
             setThemeColorConditional(<IoLeaf/>)
         }else if(number === 3){
             bg = 'rgba(255, 255, 255, 1)'
             text = 'rgba(138, 170, 229, 1)'
             setThemeColorConditional(<FaCloud/>)
+        }else if(number === 4){
+            bg = 'rgba(241, 211, 211, 1)'
+            text = 'rgb(223, 47, 50)'
+            setThemeColorConditional(<GiRose/>)
         }else {
             bg = 'rgba(26, 27, 32, 1)'
             text = 'rgba(255, 255, 255, 1)'
@@ -46,7 +51,7 @@ function NavBar() {
             </div>
             <div className='menu'>
                 <motion.ul className='menuList' initial={{y: -10}} whileInView={{y: 0}} transition={{ duration: 1 }} viewport={{ amount: 0 }} >
-                    <li  onClick={() => setNumber(prevValue => prevValue < 4 ? prevValue + 1 : 1)}>{themeColorConditional}</li>
+                    <li  onClick={() => setNumber(prevValue => prevValue < 5 ? prevValue + 1 : 1)}>{themeColorConditional}</li>
                     <li onClick={() => {scrollContext.setValue(0);scrollContext.setCon(90000)}}>Home</li>
                     <li onClick={() => {scrollContext.setValue(1);scrollContext.setCon(90000)}}>Project</li>
                     <li onClick={() => {scrollContext.setValue(2);scrollContext.setCon(90000)}}>Skills</li>
